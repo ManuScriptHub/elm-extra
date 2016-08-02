ff4886309bcdf131f03ef3916d533e2794ad70bb
Manuel Baumann <mbaumann@gmail.com>
2016-08-02 23:19:56 +0200

    progress

--- Extra/Vec2.elm
+++ Extra/Vec2.elm
@@ -1,4 +1,4 @@
-module Extra.Vec2 exposing (origin, xUnit, yUnit, toStringTuple)
+module Extra.Vec2 exposing (origin, xUnit, yUnit, toStringTuple, fromSize, toIntTuple, rotate)
 
 import Math.Vector2 exposing (Vec2, vec2, getX, getY)
 
@@ -29,3 +29,54 @@ toStringTuple v =
     ( toString <| getX v
     , toString <| getY v
     )
+
+
+fromSize : { width : Int, height : Int } -> Vec2
+fromSize size =
+    vec2 (toFloat size.width) (toFloat size.height)
+
+
+toIntTuple : Vec2 -> ( Int, Int )
+toIntTuple v =
+    ( round <| getX v
+    , round <| getY v
+    )
+
+
+rotate : Vec2 -> Float -> Vec2
+rotate v g =
+    let
+        ix =
+            getX v
+
+        iy =
+            getY v
+
+        r =
+            g * 0.017453293
+
+        s =
+            sin r
+
+        c =
+            cos r
+
+        sx =
+            s * ix
+
+        cx =
+            c * ix
+
+        sy =
+            s * iy
+
+        cy =
+            c * iy
+
+        tx =
+            cx - sy
+
+        ty =
+            sx + cy
+    in
+        vec2 tx ty
